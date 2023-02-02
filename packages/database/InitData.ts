d6e4e4cab56255f44c2e250703001aac9b5d3d31
Arthur Lee <arthur.lee945@gmail.com>
2023-02-01 20:27:30 -0600

    update

---
+++ packages/database/InitData.ts
@@ -0,0 +1,12 @@
+import { prisma } from "./index";
+
+// (async () => {
+//   const newUser = await prisma.user.create({
+//     data: {
+//       username: "test",
+//       email: "test@test.com",
+//       password: "test",
+//     },
+//   });
+//   console.log(newUser);
+// })();
