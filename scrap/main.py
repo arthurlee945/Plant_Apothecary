991d2cab40f4fb4e7ac0264a6c5d12b3899c76c1
Arthur Lee <83962300+arthurlee945@users.noreply.github.com>
2023-02-02 16:55:40 -0600

    Update main.py

--- scrap/main.py
+++ scrap/main.py
@@ -1,10 +1,5 @@
-from scraper import SteamScrapper
+from scraper import Scrapper
 
 
 if __name__ == "__main__":
-    s_scrapper = SteamScrapper()
-    to_email = input("where would you like the email to sent to?: ")
-    while "@" not in to_email or "." not in to_email:
-        to_email = input("Please enter valid email: ")
-
-    s_scrapper.send_data_to_email(to_addr=to_email)
+    s_scrapper = Scrapper()
