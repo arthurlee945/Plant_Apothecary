eb3fa2d7d9064472b10e79bd6229f24a07d6386f
Arthur Lee <83962300+arthurlee945@users.noreply.github.com>
2023-02-03 14:40:14 -0600

    Update scraper.py

--- scrap/scraper.py
+++ scrap/scraper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common import NoSuchElementException, TimeoutException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,15 +12,10 @@ class Scrapper:
     def __init__(self):
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    def get_data(self) -> str:
-        URL = "https://ipm.ucanr.edu/PMG/diseases/diseaseslist.html"
-        Base_URL = "https://ipm.ucanr.edu"
-        self.driver.get(URL)
-        listings = self.driver.find_elements(By.CSS_SELECTOR, "table[id='ALLDISEASES'] tbody tr")
-        file_name = "plant_diseases.csv"
+    def processor(self, tr):
         keys = [key.lower() for key in ["Hosts", "Symptoms", "Conditions favoring disease", "Prevention and management", "Identification", "Life cycle",
                 "Solutions", "DAMAGE", "IDENTIFICATION AND BIOLOGY", "INFECTION", "Viruses"]]
-        tr = listings[5]
+        dict_text = {key: "" for key in keys}
         tds = tr.find_elements(By.TAG_NAME, 'td')
         host = tds[0].text
         cn_td = tds[1].find_element(By.TAG_NAME, 'a')
@@ -28,80 +24,80 @@ class Scrapper:
         dType = tds[3].text
         cn_td.click()
         try:
-            info_tb = WebDriverWait(self.driver, timeout=3).until(lambda d: d.find_elements(By.CSS_SELECTOR, "table[class='layout'] tbody tr td"))
-            if len(info_tb) == 2:
-                if info_tb[0].find_element(By.TAG_NAME, 'img'):
-                    img_lnks = ", ".join([img.get_attribute('src') for img in info_tb[0].find_elements(By.TAG_NAME, "img")])
+            try:
+                init = WebDriverWait(self.driver, timeout=3).until(
+                    lambda d: d.find_element(By.CSS_SELECTOR, "table[class='layout'] >tbody >tr"))
+                info_tb = init.find_elements(By.XPATH, "./*")
+                try:
+                    info_tb[0].find_element(By.TAG_NAME, "img")
+                    img_lnks = "<<SPLIT>>".join(
+                        [img.get_attribute('src') for img in info_tb[0].find_elements(By.TAG_NAME, "img")])
                     info_text = info_tb[1].text
-                else:
-                    img_lnks = ", ".join([img.get_attribute('src') for img in info_tb[1].find_elements(By.TAG_NAME, "img")])
+                except NoSuchElementException:
+                    info_tb[1].find_element(By.TAG_NAME, "img")
+                    img_lnks = "<<SPLIT>>".join(
+                        [img.get_attribute('src') for img in info_tb[1].find_elements(By.TAG_NAME, "img")])
                     info_text = info_tb[0].text
-            else:
-                if info_tb.find_element(By.TAG_NAME, 'img'):
-                    img_lnks = ", ".join([img.get_attribute('src') for img in info_tb.find_elements(By.TAG_NAME, "img")])
-                    info_text = "No Information Available"
-                else:
-                    info_text = info_tb.text
-
-            dict_text = {key:"" for key in keys}
-            filtered_info_text = "<<NL>>".join([txt for txt in info_text.split("\n") if
-                                  "For more information" not in txt
-                                  and "Pest Notes:" not in txt])
+            except TimeoutException:
+                one_item = WebDriverWait(self.driver, timeout=3).until(
+                    lambda d: d.find_element(By.CSS_SELECTOR, "td[id='contentarea'] "
+                                                              ">table[border='0']:not(table[class='menuarea']):not(table[id='tableofcontents']) >tbody >tr >td"))
+                img_lnks = ""
+                info_text = one_item.text
 
             split_text = [txt.lower() for txt in info_text.split("\n") if
-                                  "For more information" not in txt
-                                  and "Pest Notes:" not in txt]
-
-            # if split_text[0] not in keys and len(split_text) >=2:
-            #     dictText["title"] = {
-            #         split_text[0] :
-            #     }
-            print(split_text)
-            try:
-                current = {}
-                using = 0
-                arr = []
-                for k in keys:
-                    if k in split_text:
+                          "For more information" not in txt
+                          and "Pest Notes:" not in txt]
+            current = {}
+            using = 0
+            for k in keys:
+                if k in split_text:
+                    if using != k:
                         s_i = split_text.index(k)
-                        if using != k:
-                            current[k] = [s_i]
-                            if using != 0:
-                                currList = current[using]
-                                currList.append(s_i - 1)
-                                current[using] = currList
-                        using = k
+                        current[k] = [s_i + 1]
+                        if using != 0:
+                            currList = current[using]
+                            currList.append(s_i - 1)
+                            current[using] = currList
+                    using = k
+            f_curr_pos = current[list(current.keys())[0]][0]
+            last = [last for i, last in enumerate(current) if i == len(current) - 1][0]
+            current[last] = [current[last][0], len(split_text) - 1]
+            if split_text[0] not in keys and len(split_text) >= 2:
+                dict_text["title"] = {
+                    split_text[0]: " ".join([txt for i, txt in enumerate(split_text) if i >= 1 and i < f_curr_pos - 1])
+                }
+            else:
+                dict_text["title"] = {"": ""}
 
-                print(current, "why not")
-            except:
-                print('failed')
+            for k in current:
+                dict_text[k] = " ".join([txt for i, txt in enumerate(split_text)
+                                         if i >= current[k][0] and i <= current[k][1]])
+        except (NoSuchElementException, TimeoutException, IndexError):
+            img_lnks = "<<SPLIT>>".join([img.get_attribute('src') for img in self.driver.find_elements(By.CSS_SELECTOR,
+                                                                                                "table[id='pagecolumns'] tbody tr div[class='pnphotobox'] img")])
+            dict_text["title"] = {"<<VISIT>>" + str(self.driver.current_url) : ""}
 
+        return [host, common_name, scientific_name, dType, img_lnks, dict_text["hosts"],
+                dict_text["symptoms"], dict_text["conditions favoring disease"], dict_text["prevention and management"],
+                dict_text["identification"], dict_text["life cycle"], dict_text["solutions"], dict_text["damage"],
+                dict_text["identification and biology"], dict_text["infection"], dict_text["viruses"], list(dict_text['title'].keys())[0], list(dict_text['title'].values())[0]]
 
+    def get_data(self) -> str:
+        URL = "https://ipm.ucanr.edu/PMG/diseases/diseaseslist.html"
+        Base_URL = "https://ipm.ucanr.edu"
+        self.driver.get(URL)
+        listings = self.driver.find_elements(By.CSS_SELECTOR, "table[id='ALLDISEASES'] tbody tr")
+        file_name = "plant_diseases.csv"
 
-        except:
-            img_lnks = ", ".join([img.get_attribute('src') for img in self.driver.find_elements(By.CSS_SELECTOR,
-                                                                                            "table[id='pagecolumns'] tbody tr div[class='pnphotobox'] img")])
-            info_text = "<<VISIT>>" + str(self.driver.current_url)
+        rows = ['plant or crop host', 'common name', 'scientific name', 'type', "imgLinks", "hosts", 'symptoms',
+                "conditions favoring disease", "prevention and management", "identification", "life cycle", "solutions",
+                "damage", "identification and biology", "infection", "viruses", "title name", "title description"]
 
-        # print(host, common_name, scientific_name, dType, "moved",img_lnks , filtered_info_text)
-
-
-
-        # with open(file_name, 'w', encoding='UTF8') as file:
-        #     writer = csv.write(file)
-        #     writer.writerow(['plant or Crop Host', 'Common name', 'Scientific name', 'Type', ])
-        #     for tr in listings:
-        #         title = tr.find_elements(By.CSS_SELECTOR, "a")[1].text
-        #         peak = int(tr.find_elements(By.CSS_SELECTOR, "td")[-2].text.replace(",", ""))
-        #         writer.writerow([title, peak])
-        # return file_name
-        while True:
-            pass
-
-    # def send_data_to_email(self, to_addr: str):
-    #     file_name = self.get_data()
-    #     with smtplib.SMTP("smtp.gmail.com") as session:
-    #         session.starttls("jlee24281@gmail.com", "fdjdjbwngoyrdapn")
-    #         session.login()
-    #         msg = MIMEMultipart()
-    #         msg
+        with open(file_name, 'w', encoding='UTF8', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(rows)
+            for i,tr in enumerate(listings):
+                print(i)
+                writer.writerow(self.processor(tr))
+                self.driver.back()
